98396248ab0ffa313ef7da1a23d022a819ec4444
vimal336 <asvimal336@gmail.com>
2025-09-18 12:53:09 +0530

    initial commit

---
+++ DemoApi/Model/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Model;
+
+namespace DemoApi
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
